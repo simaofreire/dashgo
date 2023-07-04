3853a34cc6c6ddae003bb7f22fa1dec4806ff74f
Simão Freire <simaofreire@live.com>
2023-07-03 22:17:33 -0300

    mirage and faker config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import { Roboto } from 'next/font/google';
 import { ChakraProvider } from './providers/ChakraProvider';
+import { makeServer } from './services/mirage';
 
 const roboto = Roboto({
 	weight: ['400', '500', '700'],
 	subsets: ['latin']
 });
+
+if (process.env.NODE_ENV === 'development') {
+	makeServer();
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
